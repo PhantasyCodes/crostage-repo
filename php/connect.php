15da264aaeb74360c9a4b50fe5c444aba6268f58
David King <davidkingmwai@gmail.com>
2022-05-27 09:57:55 +0300

    linking

--- php/connect.php
+++ php/connect.php
@@ -1,16 +1,10 @@
 <?php
 
-$server_name = "localhost:3306";
+$server_name = "localhost";
 $username = "crostage_admin";
 $password = "D@v1dK1ng";
 $database="crostage_db";
 
 $connection = mysqli_connect($server_name, $username, $password, $database);
-
-if (!$conn) {
-    die("Failed".mysqli_connect_error());
-}else {
-    echo "Connected Successfully";
-}
 
 ?>
--- php/process_register.php
+++ php/process_register.php
@@ -1,6 +1,9 @@
 <?php
 
-print_r($_POST);
+require("connect.php");
+
+// print_r($_POST);
+
 
 $first_name = $_POST["firstname"];
 $last_name = $_POST["lastname"];
@@ -8,3 +11,4 @@ $username = $_POST["username"];
 $number = $_POST["number"];
 $email = $_POST["email"];
 $password = $_POST["password"];
+
