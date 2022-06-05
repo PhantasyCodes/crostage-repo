2f807feca34277842d02f136746b12c3b7f908da
David King <davidkingmwai@gmail.com>
2022-06-05 23:47:02 +0300

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -7,10 +7,10 @@ $database="crostage_db";
 
 $connection = mysqli_connect($server_name, $username, $password, $database);
 
-if (!$connection) {
-    die("Failed".mysqli_connect_error());
-}else {
-    echo "Connected Successfully";
-}
+// if (!$connection) {
+//     die("Failed".mysqli_connect_error());
+// }else {
+//     echo "Connected Successfully";
+// }
 
 ?>
