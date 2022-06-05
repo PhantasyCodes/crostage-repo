f417e84513ede47fa46d650719a93d30add24443
David King <davidkingmwai@gmail.com>
2022-06-05 23:39:29 +0300

    Update product_register.php

--- admin/product_register.php
+++ admin/product_register.php
@@ -5,10 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crostaǵe</title>
-    <link rel="icon" type="image/x-icon" href="images/bird.png">
+    <link rel="icon" type="image/x-icon" href="../images/bird.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
@@ -43,6 +41,7 @@
                 $category_sql = "SELECT category_id, category_name FROM categories";
 
                 $category_table = mysqli_query($connection, $category_sql);
+                echo "<h1>Nice</h1>";
 
 
                 for($i = 0; $i <= mysqli_num_rows($category_table); $i++){
