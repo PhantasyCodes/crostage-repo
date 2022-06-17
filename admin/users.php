31d3e8f53012e110c35cd827e94ac22bfefff343
David King <davidkingmwai@gmail.com>
2022-06-17 20:21:20 +0300

    Update

--- admin/users.php
+++ admin/users.php
@@ -7,4 +7,13 @@ $sql = "SELECT * FROM users";
 
 $result = mysqli_query($connection, $sql);
 
-print_r($result);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  
+  mysqli_close($conn);
