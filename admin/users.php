8739d0cc23456c69c892e478dda30bf292ee53ae
David King <davidkingmwai@gmail.com>
2022-06-17 20:23:09 +0300

    Update users.php

--- admin/users.php
+++ admin/users.php
@@ -10,7 +10,7 @@ $result = mysqli_query($connection, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+      echo $row . "<br>";
     }
   } else {
     echo "0 results";
