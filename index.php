762039c8119e5e366b8c25dc19755de9efaf6810
David King <davidkingmwai@gmail.com>
2022-06-24 17:20:55 +0300

    Nice

--- index.php
+++ index.php
@@ -206,40 +206,9 @@
                 or visit our store, and we’ll help you make a wonderful selection</h2>
             <img src="images/icons-sec-4.png" alt="">
         </div>
-        <div class="footer">
-            <div class="col col-1">
-                <h1>Need Help?</h1>
-                <a href="">+254700405609</a>
-                <a href="">Delivery and returns</a>
-                <a href="">FAQ's</a>
-                <a href="">Contact us</a>
-            </div>
-            <div class="col col-2">
-                <h1>Our Organisation</h1>
-                <a href="">About us</a>
-                <a href="">Our location</a>
-                <a href="">Terms and conditions</a>
-                <a href="">Privacy policy</a>
-            </div>
-            <div class="col col-3">
-                <h1>Our Partners</h1>
-                <a href="">Fashion Stars</a>
-                <a href="">Printz Collections</a>
-                <a href="">Alphago</a>
-            </div>
-            <div class="col col-4">
-                <h1>More Links</h1>
-                <a href="">Testimonies</a>
-                <a href="">Fashion Gala 2022</a>
-                <a href="">Jobs</a>
-                <a href="">Leave a note</a>
-            </div>
-            <div class="col-5">
-                <i class="fa fa-instagram fa-lg"></i>
-                <i class="fa fa-whatsapp fa-lg"></i>
-                <i class="fa fa-envelope-o fa-lg"></i>
-            </div>
-        </div>
+        <?php
+        include_once 'footer.php';
+        ?>
     </div>
     
     
