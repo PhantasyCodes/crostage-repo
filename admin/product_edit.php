83d69a568a8ed40c6e4908563b67513397b5d255
David King <davidkingmwai@gmail.com>
2022-06-24 08:55:25 +0300

    Update product_edit.php

--- admin/product_edit.php
+++ admin/product_edit.php
@@ -1,3 +1,31 @@
-<?php
-
-print_r($_REQUEST['id']);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crostaǵe</title>
+    <link rel="icon" type="image/x-icon" href="images/bird.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="product-page-wrapper">
+        <div class="side-panel">
+            <img src="../images/logo-white.png" alt="">
+            <div class="side-panel-links">
+                <a href=""><button class="big-btn side-btns">Dashboard</button></a>
+                <a href="users.php"><button class="big-btn side-btns">Users</button></a>
+                <a href=""><button class="big-btn side-btns btn-active">Products</button></a>
+                    <a href="product_register.php"><button class="side-btns sub-button">Add New</button></a>
+                <a href="categories.php"><button class="big-btn side-btns">Categories</button></a>
+                <a href=""><button class="big-btn side-btns">Sign Out</button></a>
+            </div>
+        </div>
+        <div class="product-content">
+        </div>
+    </div>
+    <script src="../js/jquery-3.6.0.min.js"></script>
+    <script src="../js/admin.js"></script>
+</body>
+</html>
