84640233d8aae58ad537c3d5e3c031db86acb099
David King <davidkingmwai@gmail.com>
2022-06-24 19:28:03 +0300

    Update process_register.php

--- php/process_register.php
+++ php/process_register.php
@@ -13,6 +13,12 @@ $email = $_POST["email"];
 $password = $_POST["password"];
 $filename = $_FILES['file']['name'];
 
+$location = "../images/".$filename;
+
+if( move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
+    echo 'File uploaded succesfully';
+}
+
 $sql = "INSERT INTO users (first_name, last_name, username, profile_picture, phone_no, email, password, type) 
 VALUES ('$first_name', '$last_name', '$username', '$filename', '$number', '$email', '$password', 'user')";
 
