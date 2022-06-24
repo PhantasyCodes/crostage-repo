84a9af9f52ebcca0f468c1cbee4d5ad2a1657c18
David King <davidkingmwai@gmail.com>
2022-06-24 19:42:07 +0300

    Update process_register.php

--- php/process_register.php
+++ php/process_register.php
@@ -1,25 +1,32 @@
 <?php
 
-require("connect.php");
 
-// print_r($_POST);
+if (isset($_POST["submit"])) {
+    require("connect.php");
+
+    // print_r($_POST);
 
 
-$first_name = $_POST["firstname"];
-$last_name = $_POST["lastname"];
-$username = $_POST["username"];
-$number = $_POST["number"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-$filename = $_FILES['file']['name'];
+    $first_name = $_POST["firstname"];
+    $last_name = $_POST["lastname"];
+    $username = $_POST["username"];
+    $number = $_POST["number"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $filename = $_FILES['file']['name'];
 
-$location = "../images/".$filename;
+    $location = "../images/".$filename;
 
-if( move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
-    echo 'File uploaded succesfully';
+    if( move_uploaded_file($_FILES['file']['tmp_name'], $location)) {
+        echo 'File uploaded succesfully';
+    }
+
+    $sql = "INSERT INTO users (first_name, last_name, username, profile_picture, phone_no, email, password, type) 
+    VALUES ('$first_name', '$last_name', '$username', '$filename', '$number', '$email', '$password', 'user')";
+
+    mysqli_query($connection, $sql);
 }
 
-$sql = "INSERT INTO users (first_name, last_name, username, profile_picture, phone_no, email, password, type) 
-VALUES ('$first_name', '$last_name', '$username', '$filename', '$number', '$email', '$password', 'user')";
-
-mysqli_query($connection, $sql);
+else {
+    header("location: ../index.php");
+}
