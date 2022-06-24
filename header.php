6d6260e978abe90c52be70b27433dd41aeabe18b
David King <davidkingmwai@gmail.com>
2022-06-24 17:17:56 +0300

    Header

---
+++ header.php
@@ -0,0 +1,17 @@
+<nav class="navbar">
+        <div class="container">
+            <div class="search-bar">
+              <form action="/action_page.php" class="search-form">
+                  <input type="text" placeholder="Find something..." name="search" >
+                  <i class="fa fa-search"></i>
+              </form>
+            </div>
+            <div class="logo-wrapper">
+                <a href="#"><img class=logo src="images/logo-black.png" alt=""></a>
+            </div>
+            <div class="nav-links">
+                <a class="signin-link" href="signin.php">SIGN IN</a>
+                <a href=""><i class="fa fa-shopping-bag" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -15,23 +15,9 @@
 <body>
     <div id="anim" class="loader">
     </div>
-    <nav class="navbar">
-        <div class="container">
-            <div class="search-bar">
-              <form action="/action_page.php" class="search-form">
-                  <input type="text" placeholder="Find something..." name="search" >
-                  <i class="fa fa-search"></i>
-              </form>
-            </div>
-            <div class="logo-wrapper">
-                <a href="#"><img class=logo src="images/logo-black.png" alt=""></a>
-            </div>
-            <div class="nav-links">
-                <a class="signin-link" href="signin.php">SIGN IN</a>
-                <a href=""><i class="fa fa-shopping-bag" aria-hidden="true"></i></a>
-            </div>
-        </div>
-    </nav>
+    <?php
+    include_once 'header.php';
+    ?>
     <div id="butter">
         <div class="hero">
             <div class="hero-left">
