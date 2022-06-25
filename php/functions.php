fec5916d6f8d441d24472e064f2be50f2a05e7c7
David King <davidkingmwai@gmail.com>
2022-06-25 17:08:30 +0300

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -70,7 +70,7 @@ function createUser($connection, $first_name, $last_name, $username, $number, $e
 
     $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
 
-    mysqli_stmt_bind_param($query, "sssssss", $first_name, $last_name, $username, $number, $email, $hashedPwd, $filename);
+    mysqli_stmt_bind_param($query, "sssssss", $first_name, $last_name, $username, $filename, $number, $email, $hashedPwd, $filename);
     mysqli_stmt_execute($query);
     mysqli_stmt_close($query);
     header("location: ../signin.php?error=none");
