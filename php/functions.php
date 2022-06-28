bf3c301fde9f9489c70f7e92095e3937b46d8d5e
David King <davidkingmwai@gmail.com>
2022-06-28 12:37:48 +0300

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -88,7 +88,7 @@ function emptyInputSignin($username, $pwd) {
 }
 
 function loginUser($connection, $username, $pwd) {
-    echo "pwd";
+    echo "$pwd";
     return;
     $uidExists = takenUid($connection, $username, $username);
 
