ab65bfc73342de4c81a1e4d3102c58ec1e73806d
David King <davidkingmwai@gmail.com>
2022-06-29 21:37:22 +0300

    Update shop.php

--- shop.php
+++ shop.php
@@ -32,7 +32,11 @@
                 // output data of each row
                 while($row = mysqli_fetch_assoc($result)) {
                     $id = $row['product_id'];
-                    echo "<div class='shop-item' data-href='http://www.crostage.co.ke/admin/product_edit.php?id=$id'><img class='admin-img' src='images/" . $row['product_img'] . "><h1>" . $row["product_name"] . "</h1><h2>" . $row["product_price"] . "</h2></div>";
+                    echo "<div class='shop-item'>";
+                    echo "<img class='admin-img' src='images/" . $row['product_img'] . ">";
+                    echo "<h1>" . $row["product_name"] . "</h1>";
+                    echo "<h2>" . $row["product_price"] . "</h2>";
+                    echo "</div>";
                 }
             } else {
                 echo "0 results";
