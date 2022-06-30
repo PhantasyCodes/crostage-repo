f35a440774241583310c53c76293db4d079a236f
David King <davidkingmwai@gmail.com>
2022-07-01 00:09:51 +0300

    Update shop.php

--- shop.php
+++ shop.php
@@ -21,53 +21,75 @@
     ?>
     <div class="shop-container">
         <div class="shop-sidebar">
+            <?php
+                require("php/connect.php");
+
+                $sql = "SELECT products.product_id, products.product_img, products.product_name, products.product_type, products.product_price, categories.category_name FROM products INNER JOIN category_item ON products.product_id = category_item.product_id INNER JOIN categories ON categories.category_id = category_item.category_id;";
+
+                $result = mysqli_query($connection, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($result)) {
+                        $category = $row['category_name'];
+                        echo "<form method='post'>";
+                        echo "<input type='checkbox' name='$category' value='$category'>";
+                        echo "<label for='$category'>$category<label><br>";
+                        echo "</form>";
+                    }
+                } else {
+                    echo "0 results";
+                }
+
+                mysqli_close($conn);
+            ?>
 
         </div>
         <div class="shop-content">
             <?php
-            require("php/connect.php");
+                require("php/connect.php");
 
-            $sql = "SELECT products.product_id, products.product_img, products.product_name, products.product_type, products.product_price, categories.category_name FROM products INNER JOIN category_item ON products.product_id = category_item.product_id INNER JOIN categories ON categories.category_id = category_item.category_id;";
+                $sql = "SELECT products.product_id, products.product_img, products.product_name, products.product_type, products.product_price, categories.category_name FROM products INNER JOIN category_item ON products.product_id = category_item.product_id INNER JOIN categories ON categories.category_id = category_item.category_id;";
 
-            $result = mysqli_query($connection, $sql);
+                $result = mysqli_query($connection, $sql);
 
-            if (mysqli_num_rows($result) > 0) {
-                // output data of each row
-                while($row = mysqli_fetch_assoc($result)) {
-                    $id = $row['product_id'];
-                    echo "<div class='shop-item'>";
-                    echo "<img class='admin-img' src='images/" . $row['product_img'] . "'>";
-                    echo "<div class='shop-item-child'>";
-                    echo "<div class='shop-item-child-left'>";
-                    echo "<h1 style='font-weight:400;'>" . $row["product_name"] . "</h1>";
-                    echo "<h2 style='font-weight:300;'>" . $row["product_type"] . "</h2>";
-                    echo "<h2 style='font-weight:300;'>" . $row["product_price"] . "</h2>";
-                    echo "</div>";
-                    echo "<div class='shop-item-child-right'>";
-                    echo "<button class='shadow-btn'>VIEW</button>";
-                    echo "<form method='post'>";
-                    echo "<button action='submit' class='shadow-btn' name='button' value='" . $id . "'>ADD TO CART</button>";
-                    echo "</form>";
-                    echo "</div>";
-                    echo "</div>";
-                    echo "</div>";
+                if (mysqli_num_rows($result) > 0) {
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['product_id'];
+                        echo "<div class='shop-item'>";
+                        echo "<img class='admin-img' src='images/" . $row['product_img'] . "'>";
+                        echo "<div class='shop-item-child'>";
+                        echo "<div class='shop-item-child-left'>";
+                        echo "<h1 style='font-weight:400;'>" . $row["product_name"] . "</h1>";
+                        echo "<h2 style='font-weight:300;'>" . $row["product_type"] . "</h2>";
+                        echo "<h2 style='font-weight:300;'>" . $row["product_price"] . "</h2>";
+                        echo "</div>";
+                        echo "<div class='shop-item-child-right'>";
+                        echo "<button class='shadow-btn'>VIEW</button>";
+                        echo "<form method='post'>";
+                        echo "<button action='submit' class='shadow-btn' name='button' value='" . $id . "'>ADD TO CART</button>";
+                        echo "</form>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                } else {
+                    echo "0 results";
                 }
-            } else {
-                echo "0 results";
-            }
 
-            $userId = $_SESSION["userid"];
-            
-            if(isset($_POST["button"])) {
-                $prodId = $_POST["button"];
-                $sql2 = "INSERT INTO cart_items (cart_id, product_id, quantity, is_active) VALUES ('$userId', '$prodId', '1', 'yes')";
+                $userId = $_SESSION["userid"];
+                
+                if(isset($_POST["button"])) {
+                    $prodId = $_POST["button"];
+                    $sql2 = "INSERT INTO cart_items (cart_id, product_id, quantity, is_active) VALUES ('$userId', '$prodId', '1', 'yes')";
 
-                mysqli_query($connection, $sql2);
-            }
+                    mysqli_query($connection, $sql2);
+                }
 
-            unset($_POST["button"]);
+                unset($_POST["button"]);
 
-            mysqli_close($conn);
+                mysqli_close($conn);
             ?>
         </div>
     </div>
