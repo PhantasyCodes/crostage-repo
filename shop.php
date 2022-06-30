e7e8846190bec4432969cb99c067f24c66fa4b58
David King <davidkingmwai@gmail.com>
2022-07-01 00:51:53 +0300

    Update shop.php

--- shop.php
+++ shop.php
@@ -48,9 +48,11 @@
                     echo "<button action='submit' form='category-form' class='shadow-btn'>SUBMIT</button>";
 
                     if(isset($_POST["choice"])) {
+                        $choice = $_POST["choice"];
                         $sql = "SELECT products.product_id, categories.category_name FROM products INNER JOIN category_item ON products.product_id = category_item.product_id INNER JOIN categories ON categories.category_id = category_item.category_id  WHERE categories.category_name = '$category';";
                         
-                        header("location: shop.php?category=$category");
+                        header("location: shop.php?category='$choice'");
+                        echo "nice";
                         mysqli_query($connection, $sql2);
                     }
 
