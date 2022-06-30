d6164af2570d8d8198a151b1113ecf1fbee18b3f
David King <davidkingmwai@gmail.com>
2022-07-01 01:04:37 +0300

    Update shop.categories.php

--- php/shop.categories.php
+++ php/shop.categories.php
@@ -1,12 +1,11 @@
 <?php
 require("php/connect.php");
-
 if(isset($_POST["choice"])) {
     $choice = $_POST["choice"];
     $sql = "SELECT products.product_id, categories.category_name FROM products INNER JOIN category_item ON products.product_id = category_item.product_id INNER JOIN categories ON categories.category_id = category_item.category_id  WHERE categories.category_name = '$category';";
-    
-    header("location: shop.php?category='$choice'");
-    echo "nice";
-    mysqli_query($connection, $sql2);
+    mysqli_query($connection, $sql);
     mysqli_close($conn);
+    header("location: shop.php?category='$choice'");
+    exit();
+    
 }
