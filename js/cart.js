e34d5839e10ea0ceb8d2dbd70c56a0c4f3571e92
David King <davidkingmwai@gmail.com>
2022-07-05 01:27:55 +0300

    Update

--- js/cart.js
+++ js/cart.js
@@ -5,7 +5,8 @@ function stepper(btn) {
     let max = myInput.getAttribute("max");
     let step = myInput.getAttribute("step");
     let val = myInput.getAttribute("value");
-    let calcStep = (id == "increment") ? (step * 1) : (step * -1);
+    let calcStep = (id == "increment") ? (step * 1) :
+    (step * -1);
     let newValue = parseInt(val) + calcStep;
 
     if(newValue >= min && newValue <= max) {
