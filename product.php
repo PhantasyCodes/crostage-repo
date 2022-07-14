9b68e9060306a0c2872aa8ca36f375cd1253745c
David King <davidkingmwai@gmail.com>
2022-07-14 17:57:58 +0300

    Update

--- product.php
+++ product.php
@@ -17,12 +17,12 @@
     <?php
     require ("header.php");
     ?>
-    <div class="slider">
+    <div class="slider product">
         <div class="slider-description">
             <h2>HOODIES</h2>
             <h1>Hunger Games</h1>
             <h3>Ksh 1000</h3>
-            <p>Product description is what describes what the product really is. Like dont you get I’m just filling space by now?</p>
+            <p>Product description is what describes what the product really is. Like dont you get I'm just filling space by now?</p>
             <a href="shop.php"><button class="shadow-btn slider-btn">SHOP NOW</button></a>
         </div>
         <div class="slider-image">
