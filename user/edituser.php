d7fd84cf308837141980a70744dcd256a8d8a9b5
David King <davidkingmwai@gmail.com>
2022-07-14 14:15:50 +0300

    Update edituser.php

--- user/edituser.php
+++ user/edituser.php
@@ -13,23 +13,9 @@
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <nav class="navbar">
-        <div class="container">
-            <div class="search-bar">
-              <form action="/action_page.php" class="search-form">
-                  <input type="text" placeholder="Find something..." name="search" >
-                  <i class="fa fa-search"></i>
-              </form>
-            </div>
-            <div class="logo-wrapper">
-                <a href="index.php"><img class=logo src="images/logo-black.png" alt=""></a>
-            </div>
-            <div class="nav-links">
-                <a class="signin-link" href="signin.php">SIGN IN</a>
-                <a href=""><i class="fa fa-shopping-bag" aria-hidden="true"></i></a>
-            </div>
-        </div>
-    </nav>
+    <?php
+    require ("header.php");
+    ?>
     <div class="hero register-hero">
         <div class="sign-in-right register-wrapper">
             <h2>Edit your Account</h2>
