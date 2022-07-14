3da1f0df1b8c2df1b64409c5ebb7dfced9078943
David King <davidkingmwai@gmail.com>
2022-07-15 00:45:43 +0300

    Update

--- php/product_delete.php
+++ php/product_delete.php
@@ -5,7 +5,7 @@ require("connect.php");
 $product_id = $_POST["prodid"];
 echo $product_id;
 
-$sql = "UPDATE products SET is_active = 'yes';";
+$sql = "UPDATE products SET is_active = 'yes' WHERE product_id = '$product_id';";
 
 mysqli_query($connection, $sql);
 mysqli_close($connection);
