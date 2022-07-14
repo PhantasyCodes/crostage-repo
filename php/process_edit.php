4d8a4abaed81ef731ce1cecd4cc53d1d5a3453f5
David King <davidkingmwai@gmail.com>
2022-07-14 19:32:12 +0300

    Update

--- php/process_edit.php
+++ php/process_edit.php
@@ -9,9 +9,11 @@ if (isset($_POST["submit"])) {
     $password = $_POST["password"];
     $sesh_id = $_POST["id"];
 
-    require_once 'functions.php';
+    echo $first_name;
 
-    editUser($connection, $first_name, $last_name, $username, $number, $email, $password, $sesh_id);
+    // require_once 'functions.php';
+
+    // editUser($connection, $first_name, $last_name, $username, $number, $email, $password, $sesh_id);
 }
 
 else {
