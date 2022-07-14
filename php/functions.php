8fc9bf90e58a095f6033d93b19ae5b0448536e69
David King <davidkingmwai@gmail.com>
2022-07-14 19:18:40 +0300

    Update

--- php/functions.php
+++ php/functions.php
@@ -77,7 +77,7 @@ function createUser($connection, $first_name, $last_name, $username, $number, $e
 function editUser($connection, $first_name, $last_name, $username, $number, $email, $password, $sesh_id) {
     // move_uploaded_file($_FILES['file']['tmp_name'], $location);
     // $sql = "UPDATE users SET first_name = 'davidking'";
-    $sql = "UPDATE users SET first_name = $first_name, last_name = $last_name, username = $username, number = $number, email = $email, password = $password WHERE user_id = '3'";
+    $sql = "UPDATE users SET first_name = '$first_name', last_name = '$last_name', username = '$username', number = '$number', email = '$email', password = '$password' WHERE users.user_id = '3'";
     mysqli_query($connection, $sql);
     header("location: ../user.php");
     exit();
