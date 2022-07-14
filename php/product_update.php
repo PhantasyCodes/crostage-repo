d677e0d049a7a1e5dce7fa935f7ead72b896d976
David King <davidkingmwai@gmail.com>
2022-07-14 22:04:44 +0300

    Update

--- php/product_update.php
+++ php/product_update.php
@@ -6,6 +6,9 @@ $prod_desc = $_POST['pdescription'];
 $prod_price = $_POST['price'];
 $stock = $_POST['stock'];
 $id = $_POST['id'];
+
+echo $prod_name;
+
 $sql = "UPDATE products SET product_name = $prod_name, product_description = $prod_desc, product_price = $prod_price, stock = $stock WHERE product_id = $id";
 mysqli_query($connection, $sql);
 mysqli_close($conn);
